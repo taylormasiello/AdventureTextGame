37d85fb033916650456ec4f448a3213dfc8acd55
Taylor Masiello <masiellotay@gmail.com>
2022-01-25 01:59:29 -0800

    Added ReadLine and charName input from player

--- AdventureTextGame/Program.cs
+++ AdventureTextGame/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using static System.Console;
+
 
 namespace AdventureTextGame
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string charName = "Hero";
+
+            WriteLine("A Brave Hero's Adventure");
+            WriteLine("----------------------------------------------");
+            WriteLine("Are you the hero we've been waiting for, with courage enough to embark on this journey?");
+            WriteLine("\nWhat is your name, so we may tell future tales of your adventure?");
+
+            charName = ReadLine();
+
+            WriteLine("\nWelcome! And a hearty cheers to " + charName + ", our new hero!");
         }
     }
 }
