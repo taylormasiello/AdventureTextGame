14bc037dd2ceebd550dbb8ab9b97b43e482b7fee
Taylor Masiello <masiellotay@gmail.com>
2022-01-25 03:06:00 -0800

    notes for future features

--- AdventureTextGame/Program.cs
+++ AdventureTextGame/Program.cs
@@ -15,3 +15,40 @@ namespace AdventureTextGame
         }
     }
 }
+
+////original Dialog method
+//static void Dialog(string message)
+//{
+//    Console.ForegroundColor = ConsoleColor.Cyan;
+//    Console.WriteLine(message);
+//    Console.ResetColor();
+//}
+
+//Console.Write("A stranger approaches. Clearing their throat to get your attention, they ask \n");
+//Dialog("Have you seen a strange creature around here?\nAbout three feet high, greenish, with fluffy hair?\n");
+//Console.Write("How do you answer? 1) Yes, 2) No, 3) Repeat the question, 4) Keep walking");
+
+////new method
+//static void Dialog(string message, string color)
+//{
+//    if (color == "red")
+//    { Console.ForegroundColor = ConsoleColor.Red; }
+//    else if (color == "green")
+//    { Console.ForegroundColor = ConsoleColor.Green; }
+//    else if (color == "yellow")
+//    { Console.ForegroundColor = ConsoleColor.Yellow; }
+//    else
+//    { Console.ForegroundColor = ConsoleColor.White; }
+
+//    Console.WriteLine(message);
+//    Console.ResetColor();
+//}
+
+//Dialog("One argument version");
+//Dialog("Two argument version with green", "green");
+//Dialog("Two argument version with yellow", "yellow");
+
+
+// The Dialog method is now overloaded; there is more than one way in which the method can be called.
+
+
--- AdventureTextGame/TitleScreen.cs
+++ AdventureTextGame/TitleScreen.cs
@@ -24,6 +24,55 @@ namespace AdventureTextGame
             WriteLine("\nWhat is your name, so we may tell future tales of your adventure?");
             charName = ReadLine();
             WriteLine("\nWelcome! And a hearty cheers to " + charName + ", our new hero!");
+            Console.Clear();
+
+            //Consider using Console.Clear() to clear the screen. This helps separate parts of a text-heavy game (such as between levels, chapters, achievements, or when a player clicks enter at a title screen).
         }
     }
 }
+
+// The code below shows a scenario in which arguments aren't needed. A property of the Character class stores a number. A random number is compared to the stored value. If the stored value is greater than or equal to the random number, the result is true. Otherwise it is false.
+
+// A narrative for this situation could be that as a player travels through the game there are opportunities for them to increase their "luck". The higher their luck, the better their chance is against "karma". A for loop is used in the example code below so you can see results that are both true and false.
+
+//class Character
+//{
+//    static int luck = 5;
+//    static Random randomNumber = new Random();
+//    public static int points = 0;
+
+//    public static bool Luck()
+//    {
+
+//        int karma = randomNumber.Next(10);
+//        bool result = false;
+
+//        if (luck >= karma)
+//        { result = true; }
+
+//        return result;
+//    }
+//}
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.Title = "Return Value Example";
+//        Console.WriteLine("Your points: " + Character.points);
+//        for (int i = 0; i < 20; i++)
+//        {
+
+//            if (Character.Luck())
+//            {
+//                Console.WriteLine("Your luck has gained you another point!");
+//                Character.points++;
+//            }
+//            else
+//            {
+//                Console.WriteLine("Your luck has failed.... you've lost a point.");
+//                Character.points--;
+//            }
+//        }
+//        Console.WriteLine("Your points: " + Character.points);
+//        Console.Read();
+//    }
