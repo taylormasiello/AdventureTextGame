aa8eb5365282ffe7b79713f1477822702f922f26
Taylor Masiello <masiellotay@gmail.com>
2022-01-25 02:52:53 -0800

    Refactor with methods within classes (as seperate files)

--- AdventureTextGame/Program.cs
+++ AdventureTextGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+// using static AdventureTextGame.TitleScreen;
 using static System.Console;
 
 
@@ -6,18 +7,11 @@ namespace AdventureTextGame
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            string charName = "Hero";
-
-            WriteLine("A Brave Hero's Adventure");
-            WriteLine("----------------------------------------------");
-            WriteLine("Are you the hero we've been waiting for, with courage enough to embark on this journey?");
-            WriteLine("\nWhat is your name, so we may tell future tales of your adventure?");
-
-            charName = ReadLine();
-
-            WriteLine("\nWelcome! And a hearty cheers to " + charName + ", our new hero!");
+            TitleScreen.gameStart();
+            TitleScreen.setCharName();
+            
         }
     }
 }
---
+++ AdventureTextGame/TitleScreen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace AdventureTextGame
+{
+    public static class TitleScreen
+    {
+        static string charName = "Hero";
+
+        public static void gameStart()
+        {
+            Console.Title = "A Brave Hero's Adventure";
+
+            WriteLine("A Brave Hero's Adventure");
+            WriteLine("----------------------------------------------");
+            WriteLine("Are you the hero we've been waiting for, with courage enough to embark on this journey?");
+            ReadKey();
+            Console.Title = "A Journey Begins...";
+            // changes title of console window; will be useful as player progresses through levels/chapters
+        }
+
+        public static void setCharName()
+        {
+            WriteLine("\nWhat is your name, so we may tell future tales of your adventure?");
+            charName = ReadLine();
+            WriteLine("\nWelcome! And a hearty cheers to " + charName + ", our new hero!");
+        }
+    }
+}
